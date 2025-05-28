d89116db4d3ca3383c429651188d90c2a5f83c86
dammarin <marinatosd@gmail.com>
2025-05-28 19:48:54 +0300

    feat: add edit task functionality

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -5,15 +5,22 @@ import type { TodoProps, Action} from "../types.ts"
 
 const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
   switch (action.type) {
-    case "ADD":{
-      const newTodo: TodoProps = {
-        id: Date.now(),
-        text: action.payload,
-      };
-      return [...state, newTodo];
-    }
+    case "ADD":
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          text: action.payload,
+        }
+      ]
     case "DELETE":
       return state.filter(todo => todo.id !== action.payload);
+    case "EDIT":
+      return state.map( todo =>
+        todo.id === action.payload.id
+        ? {...todo, text: action.payload.newText}
+        : todo
+      )
     default:
       return state;
   }
@@ -21,7 +28,6 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
 
 const Todo = () =>{
   const [todos, dispatch] = useReducer(todoReducer, []);
-
   return (
     <>
       <div className="max-w-sm mx-auto p-6">
